82cb193282d77108ee1b92379348bb6b792c9453
kartikDas074 <kartikdas209774@gmail.com>
2026-08-30 15:48:24 +0600

    try to fixing deployment issue

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: env.bool('SERVER_PROXY', true),
   url: env('PUBLIC_URL', env('URL', '')),
   app: {
     keys: env.array('APP_KEYS'),
